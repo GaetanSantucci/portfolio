326b9ce3ee3eee2faf84baabdb26dc55533a1cd8
GaetanSantucci <gaetan.santucci@outlook.com>
2022-11-10 15:33:41 +0100

    :boom: reflexion and redesign of the skill part

--- components/Header/Contact/index.js
+++ components/Header/Contact/index.js
@@ -1,5 +1,8 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { gsap } from 'gsap';
+import { useEffect } from 'react';
 import './contact.scss';
 
 import github from '/public/images/github.png'
@@ -7,11 +10,19 @@ import linkedin from '/public/images/linkedin.png'
 import twitter from '/public/images/twitter.png'
 
 export default function Contact() {
+
+  useEffect(() => {
+    gsap.to('.bounce', { duration: 0.6, x: 50, ease: "circ.in", repeat: -1, yoyo: true })
+  });
+
   return (
     <div className='navigation'>
-      <a className='email' id='joinme' href='mailto:'>
-        contact@virtualgs.net
-      </a>
+      <div className='send-mail'>
+        <div className='line'></div>
+        <a className='email bounce' id='joinme' href='mailto:'>
+          contact@virtualgs.net
+        </a>
+      </div>
       <div className='social'>
         <Link href='https://github.com/GaetanSantucci'>
           <Image className='icon animated-letter' src={github} alt='logo github' />
