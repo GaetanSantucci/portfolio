119e1d4533f09ef7a3e17b5f72145fe9a52c6986
GaetanSantucci <gaetan.santucci@outlook.com>
2022-11-22 08:06:09 +0100

    :bug: add meta device

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,19 @@
 import '/public/styles/reset.scss';
 import '/public/styles/globals.scss'
-
 import Head from './head';
+
+import { Montserrat } from '@next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['200', '400'],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head />
-      <body>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
